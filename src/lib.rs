5185351e2c279ea7e2d35b515ccb6d246c0878c1
hatakelab <htkr1126@icloud.com>
2026-01-29 11:35:04 +0900

    Spreadpages構造体にnew関数を追加

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#[derive(Default)]
+pub struct Spreadpages {
+    path: String,
+}
+
+impl Spreadpages {
+    pub fn new(path: &str) -> Self {
+        Self sp = Self.default();
+        sp.path = path.to_string();
+        sp
+    }
+}
